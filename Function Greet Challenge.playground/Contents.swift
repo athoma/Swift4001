fbfe56d225a6cad815889d34ef4a5524fb333f2a
athoma <athomaperry@yahoo.com>
2018-02-16 22:52:41 -0600

    Person UI

---
+++ Function Greet Challenge.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func converntMilesToKilometers(miles: Double) -> Double {
+    let kilometers = miles * 1.60934
+    return kilometers
+}
